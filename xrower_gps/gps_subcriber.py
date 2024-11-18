e5fef7ba7e1c6a115643af4bcdf3ca4f7e40bb1a
MinhTuNA <viminhtu.101002@gmail.com>
2024-11-18 10:01:05 +0700

    update socketio

--- xrower_gps/gps_subcriber.py
+++ xrower_gps/gps_subcriber.py
@@ -2,9 +2,24 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import requests
 import json
-url = "http://192.168.1.214:8901/api/data/1"
+
+import socketio
+
+sio = socketio.Client()
+
+
+@sio.event
+def connect():
+    print("Connected to server")
+
+@sio.event
+def message(data):
+    print("Received from server:", data)
+
+@sio.event
+def disconnect():
+    print("Disconnected from server")
 
 class GPSSubcriberNode(Node):
     def __init__(self):
@@ -12,27 +27,23 @@ class GPSSubcriberNode(Node):
         self.create_subscription(String, 'gps_topic',self.gps_listener_callback,10)
         
     def gps_listener_callback(self,msg):
-        # self.get_logger().info(f'GPS Data >> {msg.data}')
         gps_data = json.loads(msg.data)
         latitude = gps_data['latitude']
         longitude = gps_data['longitude']
         altitude = gps_data['altitude']
         speed = gps_data['speed']
         satellites = gps_data['satellites']
-        payload = {
-            "longitude": str(longitude),
-            "latitude": str(latitude),
-        }
-        response = requests.patch(url,json=payload)
-        if response.status_code == 200:
-            print(response.json())
-        else:
-            print("Có lỗi xảy ra:", response.json())
-        
+        self.get_logger().info(msg.data)
+        if sio.connected:
+            sio.emit("DataFromROS2",{
+                "latitude": latitude,
+                "longitude": longitude,
+            })
 
 def main(args=None):
     rclpy.init(args=args)
     node = GPSSubcriberNode()
+    sio.connect("http://192.168.1.214:8901")
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
